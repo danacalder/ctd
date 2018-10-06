e8c6c67e37034564b41233f352c2f1b966bdf46c
danacalder <dana.calder@gmail.com>
2018-10-06 15:58:34 +0000

    items commentedout

--- week_three/corrective_vision.rb
+++ week_three/corrective_vision.rb
@@ -8,37 +8,38 @@ class CorrectiveVision
     end
 end    
 
+# require "corrective_vision.rb"    
+# class LensesMaterial < CorrectiveVision
+# # CorrectiveVision Am I connecting them with this symbol < ?
     
-class LensesMaterial < CorrectiveVision
-# CorrectiveVision Am I connecting them with this symbol < ?
+#     def polycarbonate
+#     end
     
-    def polycarbonate
-    end
+#     def plastic
+#     end
     
-    def plastic
-    end
+#     def glass
+#     end
     
-    def glass
-    end
+#     def highindex
+#     end
     
-    def highindex
-    end
-    
-    def photochromic
-    end
-end
+#     def photochromic
+#     end
+# end
 
-class LensesTypes < CorrectiveVision
+# require "corrective_vision.rb"
+# class LensesTypes < CorrectiveVision
     
-    def singlevision
-    end
+#     def singlevision
+#     end
     
-    def bifocal
-    end
+#     def bifocal
+#     end
     
-    def trifocal
-    end
+#     def trifocal
+#     end
     
-    def progressive
-    end
-end    
+#     def progressive
+#     end
+# end    

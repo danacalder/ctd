e1add58bd6034acac0296cac20e2eb2dd7658283
danacalder <dana.calder@gmail.com>
2018-10-09 23:49:07 +0000

    add roshambo classes file

--- week_four/roshambo/roshambo_methods/roshambo_methods.rb
+++ week_four/roshambo/roshambo_methods/roshambo_methods.rb
@@ -1,11 +1,21 @@
 def play_game(choices, draws, losses, wins)
-    puts choices
-    puts draws
-    puts losses
-    puts wins
+    user_choice = gets.chomp.downcase
+    ai_choice = rand(3)
+    puts "You chose #{user_choice} and the AI chose #{choices[ai_choice]}"
+    end_game(losses, wins, draws, choices, user_choice, ai_choice)
 end
 
-def main
+def end_game(losses, wins, draws, choices, user_choice, ai_choice)
+    if user_choice == choices[ai_choice]
+        draws += 1
+        puts "Draw: #{draw}"
+        
+        elsif user_choice == choices[ai_choice -1]
+        losses += 1
+        puts "Loss: #{losses}"
+    else
+        wins += 1
+        puts "Wins: #{wins}"
     choices = ["rock", "paper", "scissors"]
     
     puts "rock, paper, scissors"

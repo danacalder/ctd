7b65dcafa03649d71cc0e4ab906cc1a1b67f3e67
danacalder <dana.calder@gmail.com>
2018-10-13 14:45:01 +0000

    user input practice

---
+++ week_1/user_input_practice/user_input_statistics.rb
@@ -0,0 +1,14 @@
+def main
+  number = []
+  loop do
+    puts 'Enter a number'
+    number = gets.chomp
+    if number == 5
+      puts 'you\'re closer'
+    else number = 5
+      puts 'not even close'
+    break if value == '20'
+  end
+main if __FILE__ == $PROGRAM_NAME
+
+# doesn't work 

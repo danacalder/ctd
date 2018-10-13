f6fb0536c27526928694a7ae6893d83776ffc399
danacalder <dana.calder@gmail.com>
2018-10-13 15:31:09 +0000

    tic tac toe practice

---
+++ week4/tic_tac_toe/class_TicTacToe.rb
@@ -0,0 +1,8 @@
+def initialize 
+    @board = Board.new
+    @player_1 = Player.new("Doctor Who",:x,@board)
+    @player_2 = Player.new("Companion", :y,@board)
+    @current_player = player_1
+end
+
+def play

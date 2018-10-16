13a11760d321a7744053062576ac3153c2dce11a
danacalder <dana.calder@gmail.com>
2018-10-16 23:26:33 +0000

    tic tac toe work

--- week4/tic_tac_toe/TicTacToe.rb
+++ week4/tic_tac_toe/TicTacToe.rb
@@ -1,10 +1,10 @@
+class TicTacToe
+
 def initialize 
     @board = Board.new
     @player_1 = Player.new("Doctor Who",:x,@board)
     @player_2 = Player.new("Companion", :y,@board)
     @current_player = player_1
 end
-
-def display_board (Board)
-    puts "#{board[0]} | #{board[1]} | #{board [2]}"
-    puts "--------------------"
+    
+end    
